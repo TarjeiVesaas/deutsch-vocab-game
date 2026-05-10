1460dc800486c98b564a5c766bf082f9fc787415
TarjeiVesaas <158510663+TarjeiVesaas@users.noreply.github.com>
2026-05-10 09:09:50 +0100

    Update database.py
    Checking for bug

--- database.py
+++ database.py
@@ -1,7 +1,9 @@
 import psycopg2
 import os
 
-DATABASE_URL = os.environ.get("postgresql://db_users_scores_user:OshPNsIguxJLk2lxpGpP8BqjVCUHJEcD@dpg-d7ub750sfn5c73fhvco0-a/db_users_scores")
+DATABASE_URL = os.environ.get("DATABASE_URL")
+
+print("DATABASE_URL =", DATABASE_URL)
 
 def get_connection():
     return psycopg2.connect(DATABASE_URL)
